9d68a450882b8c13a8da13088391117445ab9714
Yusuke <apple@yusukenoMacBook-Pro.local>
2020-02-20 19:49:24 +0900

    湿度、風速、紫外線、花粉の項目を追加

--- weather.py
+++ weather.py
@@ -1,5 +1,6 @@
 import requests, bs4
 import re
+import time
 
 res = requests.get('https://tenki.jp/forecast/3/17/4610/14203/10days.html')
 res.raise_for_status()
@@ -14,14 +15,22 @@ lines= [line.strip() for line in text.splitlines()]
 
 i = 0
 index = []
+index_time = []
 
 #月と日から日にちを抽出
 for data in lines: 
-	if(('月' in data) & ('日' in data)):
+	if(('月' in data) & ('日' in data)|('00-06' in data) | ('06-12' in data ) | ('12-18' in data ) | ('18-24' in data )):
 		index.append(i)
 	i += 1	#カウントしてインデックスを求める
+
+
 
 for n in range(len(index) -1):
 	temp = lines[index[n]+2].split('℃')	#最高気温と最低気温が同じ配列なので分解
 	#テキスト出力
-	print(lines[index[n]] + ' 天気：' + lines[index[n]+1] + ' 最高気温：' + temp[0] + "℃" + ' 最低気温：' + temp[1] + "℃" + ' 降水確率：' + lines[index[n]+3])
+	if("月" in lines[index[n]]):
+		print(lines[index[n]] + ' 天気：' + lines[index[n]+1] + ' 最高気温：' + temp[0] + "℃" + ' 最低気温：' + temp[1] + "℃" + ' 降水確率：' + lines[index[n]+3] + '　' + lines[index[n]+6] + '　' + lines[index[n]+7])
+	else:
+		print(lines[index[n]] + ' 天気：' + lines[index[n]+1] + ' 最高気温：' + temp[0] + "℃" + ' 最低気温：' + temp[1] + "℃" + ' 降水確率：' + lines[index[n]+3] + '　湿度：' + lines[index[n]+5] + '　風速：' + lines[index[n]+6])
+
+
